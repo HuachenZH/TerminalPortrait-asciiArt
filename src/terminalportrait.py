77f7c866c50a3d963867384133e423e181a6b4f1
HuachenZH <huachen.zhang.23@eigsi.fr>
2022-11-23 20:44:27 +0100

    building function isGrayscale
    what to do precisely is written in the comment. After this, next step is to build fct color to grayscale

--- src/terminalportrait.py
+++ src/terminalportrait.py
@@ -1,54 +1,74 @@
 import numpy as np
 from PIL import Image
 
+#%% functions
+def isGrayscale(array: np.array) -> bool:
+    '''
+    Check if the rgb matrix is already grayscale. 
+
+    Parameters
+    ----------
+    array : np.array
+        The rgb matrix.
+
+    Returns
+    -------
+    bool
+        Return True if array is grayscale. False if array is colored
+
+    '''
+    if array.shape == 2: # if the rgb matrix contains only two dimensions
+        return True
+    # else, three dimensions
+    # check for one given pixel, if r, g, b three values are the same
+    # if total number of pixel < 1000, check them all
+    # else check 5% of total pixels
 
 
 
+
+#%% 
+# read image
 img = Image.open("C:\\Users\\eziod\\Documents\\yy3.jpg")
+# image to rgb matrix. The matrix can be three dimensions or two.
 arr = np.array(img)
 print(arr[0][0])
-print(arr.shape)
-arr.shape[0] 
-arr.shape[1] 
+
+
 
 # print(arr[719][0])
 
-if len(arr.shape)==3:
+if len(arr.shape)==3: # rgb matrix is three dimension
+    # check if the matrix is already grayscaled. If yes, we cant reapply the color to grayscale equation
     arr2D = arr[:,:,0]
     arr2D_rescale = arr2D.copy()//37
     print(np.amax(arr2D_rescale))
 else:
     arr2D_rescale = arr.copy()//37
 
-# 6 -> @
-# 5 -> M
-# 4 -> X
-# 3 -> /
-# 2 -> ;
-# 1 -> -
-# 0 -> ,
-
-payload=''
-
-for line in arr2D_rescale:
-    for score in line:
-        if score==6:
-            payload += ",,"
-        if score==5:
-            payload += "--"
-        if score==4:
-            payload += ";;"
-        if score==3:
-            payload += "//"
-        if score==2:
-            payload += "XX"
-        if score==1:
-            payload += "MM"
-        if score==0:
-            payload += "@@"
-    payload += "\n"
 
 
-text_file = open("data4_2_b.txt", "w")
-text_file.write(payload)
-text_file.close()
+# payload=''
+
+# for line in arr2D_rescale:
+#     for score in line:
+#         if score==6:
+#             payload += ",,"
+#         if score==5:
+#             payload += "--"
+#         if score==4:
+#             payload += ";;"
+#         if score==3:
+#             payload += "//"
+#         if score==2:
+#             payload += "XX"
+#         if score==1:
+#             payload += "MM"
+#         if score==0:
+#             payload += "@@"
+#     payload += "\n"
+
+
+# text_file = open("data4_2_b.txt", "w")
+# text_file.write(payload)
+# text_file.close()
